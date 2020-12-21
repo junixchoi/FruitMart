28b7a69b1c6d0740584c21c913bf90769d1acdab
junixchoi <junix.choi@gmail.com>
2020-12-21 14:22:43 +0900

    프로퍼티로 뷰 추출

--- FruitMart/ProductRow.swift
+++ FruitMart/ProductRow.swift
@@ -10,50 +10,8 @@ import SwiftUI
 struct ProductRow: View {
     var body: some View {
         HStack {
-            // 상품 이미지
-            Image("apple")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 140)
-                .clipped()
-            
-            VStack(alignment: .leading) {
-                // 상품명
-                Text("백설공주 사과")
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .padding(.bottom, 6)
-                
-                // 상품 설명
-                Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                
-                Spacer()
-                
-                HStack(spacing: 0) {
-                    // 가격 정보와 버튼
-                    Text("₩")
-                        .font(.footnote) +
-                        Text("2100")
-                        .font(.headline)
-                    
-                    Spacer()
-                    
-                    // 하트 아이콘
-                    Image(systemName: "heart")
-                        .imageScale(.large)
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                    
-                    // 카트 아이콘
-                    Image(systemName: "cart")
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                }
-            }
-            .padding([.leading, .bottom], 12)
-            .padding([.top, .trailing])
+            productImage
+            productDescription
         }
         .frame(height: 150)
         // colorInvert - 색상 반전
@@ -62,6 +20,61 @@ struct ProductRow: View {
         .cornerRadius(6)
         .shadow(color: Color.primary.opacity(0.33), radius: 1, x: 2, y: 2)
         .padding(.vertical, 8)
+    }
+}
+
+private extension ProductRow {
+    var productImage: some View {
+        // 상품 이미지
+        Image("apple")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+
+    var productDescription: some View {
+        VStack(alignment: .leading) {
+            // 상품명
+            Text("백설공주 사과")
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 6)
+            
+            // 상품 설명
+            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+            
+            Spacer()
+            
+            footerView
+        }
+        .padding([.leading, .bottom], 12)
+        .padding([.top, .trailing])
+    }
+
+    var footerView: some View {
+        HStack(spacing: 0) {
+            // 가격 정보와 버튼
+            Text("₩")
+                .font(.footnote) +
+                Text("2100")
+                .font(.headline)
+            
+            Spacer()
+            
+            // 하트 아이콘
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+            
+            // 카트 아이콘
+            Image(systemName: "cart")
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+        }
     }
 }
 
