1da2c89df3db3c19a17771b7f29561a8493590ad
junixchoi <junix.choi@gmail.com>
2020-12-21 13:41:04 +0900

    Initial Commit

---
+++ FruitMart/FruitMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMartApp.swift
+//  FruitMart
+//
+//  Created by user on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
