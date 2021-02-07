4c189e4fded7eca1367871445ededf7719b21ec0
junixchoi <junix.choi@gmail.com>
2021-02-08 00:04:24 +0900

    20. 즐겨찾는 상품 목록 구현하기

---
+++ FruitMart/User Interface/Home/FavoriteProductScrollView.swift
@@ -0,0 +1,81 @@
+//
+//  FavoriteProductScrollView.swift
+//  FruitMart
+//
+//  Created by user on 2021/02/07.
+//
+
+import SwiftUI
+
+struct FavoriteProductScrollView: View {
+  @EnvironmentObject private var store: Store
+  @Binding var showingImage: Bool // 즐겨찾기 상품 이미지 표시 여부
+  var body: some View {
+    VStack(alignment: .leading) {
+      title // 뷰의 목적을 표현하는 제목
+      if showingImage {
+        products // 즐겨찾기 한 상품 목록
+      }
+    }
+    .padding()
+  }
+}
+
+private extension FavoriteProductScrollView {
+  var title: some View {
+    HStack(alignment: .top, spacing: 5) {
+      Text("즐겨찾는 상품")
+        .font(.headline)
+        .fontWeight(.medium)
+      
+      Symbol("arrowtriangle.up.square")
+        .padding(4)
+        .rotationEffect(Angle(radians: showingImage ? .pi : 0))
+      
+      Spacer()
+    }
+    .padding(.bottom, 8)
+    .onTapGesture {
+      self.showingImage.toggle()
+    }
+  }
+  
+  var products: some View {
+    // 즐겨찾기 상품 목록 읽어 오기
+    let favoriteProducts = store.products.filter { $0.isFavorite }
+    return ScrollView(.horizontal, showsIndicators: false) { // 인디케이터 미표시
+      HStack(spacing: 0) {
+        ForEach(favoriteProducts) { product in
+          // 상품 선택 시, 해당 상품에 대한 상세 화면으로 이동하도록 내비게이션 링크로 연결
+          NavigationLink(
+            destination: ProductDetailView(product: product)) {
+            self.eachProduct(product)
+          }
+        }
+      }
+    }
+  }
+  
+  func eachProduct(_ product: Product) -> some View {
+    GeometryReader { g in // 스크롤 뷰 내에서 위치 정보를 얻도록 지오메트리 리더 사용
+      VStack {
+        ResizedImage(product.imageName, renderingMode: .original)
+          .clipShape(Circle()) // 이미지가 원형으로 나타나도록 clipshape 적용
+          .frame(width: 90, height: 90)
+          .scaleEffect(self.scaledValue(from: g))
+      } // 스크롤 위치에 따라 크기 조정
+      // iOS 13과 iOS 14의 지오메트리 리더 뷰 정렬 위치가 달라졌으므로 조정
+      .position(x: g.size.width / 2, y: g.size.height / 2)
+    }
+    .frame(width: 105, height: 105)
+  }
+  
+  func scaledValue(from geometry: GeometryProxy) -> CGFloat {
+    let xOffset = geometry.frame(in: .global).minX - 16
+    let minSize: CGFloat = 0.9 // 최소 크기: 원래 크기의 0.9배
+    let maxSize: CGFloat = 1.1 // 최대 크기: 원래 크기의 1.1배
+    let delta: CGFloat = maxSize - minSize // 변화폭 0.2
+    let size = minSize + delta * (1 - xOffset / UIScreen.main.bounds.width)
+    return max(min(size, maxSize), minSize)
+  }
+}
--- FruitMart/User Interface/Home/Home.swift
+++ FruitMart/User Interface/Home/Home.swift
@@ -10,16 +10,17 @@ import SwiftUI
 struct Home: View {
   @EnvironmentObject private var store: Store
   @State private var quickOrder: Product? // 빠른 주문 기능으로 주문한 상품 저장
+  @State private var showingFavoriteImage: Bool = true
   
   var body: some View {
     NavigationView {
-      List(store.products) { product in
-        NavigationLink(destination: ProductDetailView(product: product)) {
-          ProductRow(quickOrder: self.$quickOrder, product: product)
+      VStack {
+        if showFavorite {
+          favoriteProducts // 구현해 둔 스크롤 뷰에 해당하는 프로퍼티
         }
-        .buttonStyle(PlainButtonStyle())
+        darkerDivider
+        productList // 기존에 있던 코드를 프로퍼티로 추출
       }
-      .navigationBarTitle("과일 마트")
     }
     .popupOverContext(item: $quickOrder, style: .blur, content: popupMessage(product:))
   }
@@ -43,5 +44,40 @@ struct Home_Previews: PreviewProvider {
   static var previews: some View {
     Preview(source: Home())
       .environmentObject(Store())
+  }
+}
+
+private extension Home {
+  var favoriteProducts: some View { // 즐겨찾는 상품 목록
+    FavoriteProductScrollView(showingImage: $showingFavoriteImage)
+      .padding(.top, 24)
+      .padding(.bottom, 8)
+  }
+  
+  var darkerDivider: some View { // 커스텀 구분선
+    Color.primary
+      .opacity(0.3)
+      .frame(maxWidth: .infinity, maxHeight: 1)
+  }
+  
+  var productList: some View { // body에 작성되어 있던 기존 코드 추출
+    List {
+      ForEach(store.products) { product in
+        // iOS 13에서 디스클로저 인디케이터를 제거하기 위한 임시 방편이
+        // iOS 14에서 동작하지 않으므로 관련 코드 제거
+        NavigationLink(destination: ProductDetailView(product: product)) {
+          ProductRow(quickOrder: $quickOrder, product: product)
+        }
+      }
+      .listRowBackground(Color.background)
+    }
+    // iOS 14.0에서는 NavigationView - VStack - List일 때 SidebarListStyle이 기본값
+    // iOS 13의 기본값이었던 PlainListStyle로 수정
+    .listStyle(PlainListStyle())
+    .background(Color.background)
+  }
+  
+  var showFavorite: Bool {
+    !store.products.filter({ $0.isFavorite }).isEmpty
   }
 }
